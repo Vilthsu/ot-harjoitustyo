9dd2c8483d477e2a696b938a50bc988d74d3081c
Vhiltsu <vhiltsu@localhost>
2020-03-31 14:41:58 +0300

    Add ProjectManager

---
+++ ProjectManager/src/main/java/projectmanager/domain/User.java
@@ -0,0 +1,5 @@
+package projectmanager.domain;
+
+public class User {
+    
+}
---
+++ ProjectManager/src/main/java/projectmanager/ui/ProjectManagerUI.java
@@ -0,0 +1,15 @@
+package projectmanager.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ProjectManagerUI extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        
+    }
+}
