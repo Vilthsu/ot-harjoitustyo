7e78aa2e0878777c92e8f605a140b8181057a86b
Vhiltsu <vhiltsu@localhost>
2020-03-31 17:26:32 +0300

    Fix ProjectManagerUI

--- ProjectManager/src/main/java/projectmanager/ui/ProjectManagerUI.java
+++ ProjectManager/src/main/java/projectmanager/ui/ProjectManagerUI.java
@@ -4,6 +4,10 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class ProjectManagerUI extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
     @Override
     public void init() throws Exception {
       // TODO
