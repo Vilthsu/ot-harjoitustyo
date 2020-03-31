f1f784b44fdc3fe6b9592038ac2de22081e2617a
Vhiltsu <vhiltsu@localhost>
2020-03-31 17:23:47 +0300

    Move the main program from ProjectManagerUI to Main

---
+++ ProjectManager/src/main/java/projectmanager/ui/Main.java
@@ -0,0 +1,7 @@
+package projectmanager.ui;
+
+public class Main {
+    public static void main(String[] args) {
+        ProjectManagerUI.main(args);
+    }
+}
--- ProjectManager/src/main/java/projectmanager/ui/ProjectManagerUI.java
+++ ProjectManager/src/main/java/projectmanager/ui/ProjectManagerUI.java
@@ -4,10 +4,6 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class ProjectManagerUI extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
     @Override
     public void init() throws Exception {
       // TODO
