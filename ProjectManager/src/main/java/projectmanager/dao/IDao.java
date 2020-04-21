1e6d30ebf09183975bb86d00c2cc1bb66b443203
Vhiltsu <vhiltsu@localhost>
2020-04-21 19:04:06 +0300

    Fix Dao

--- ProjectManager/src/main/java/projectmanager/dao/IDao.java
+++ ProjectManager/src/main/java/projectmanager/dao/IDao.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projectmanager.dao;
 
-/**
- *
- * @author Ville
- */
-public class IDao {
-    
+import java.sql.SQLException;
+import java.util.List;
+
+
+public interface IDao<T, K> {
+    void create(T object) throws SQLException;
+    T read(K key) throws SQLException;
+    T update(T object) throws SQLException;
+    void delete(K key) throws SQLException;
+    List<T> list() throws SQLException;
 }
